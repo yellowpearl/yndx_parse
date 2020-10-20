87546a69d801db8c5f0ad506a5bb61d18db4ca6c
yellowpearl <yellowpearl@yandex.ru>
2020-10-20 21:22:56 +0300

    done TODO: setup.py, requirements.txt

---
+++ conf.py
@@ -0,0 +1,5 @@
+cnf = {
+    'user_id': 'yellowpearl',
+    'key_id': '03.492073320:ca7992ee105e493f936f65436ea8230b',
+    'strict': 'True' # or False
+}
---
+++ yndx_parser.py
@@ -0,0 +1,22 @@
+import yandex_search
+from conf import cnf
+
+user_id = cnf['user_id']
+key_id = cnf['key_id']
+
+file_src = open('sources.txt', 'r')
+file_req = open('request.txt', 'r')
+
+sources = file_src.read().split('\n')
+req = file_req.read().split('\n')[0]
+
+yandex = yandex_search.Yandex(api_user=user_id, api_key=key_id)
+i = 0
+file_ans = open('ans.html', 'w')
+for source in sources:
+    q_text = f'site:{source} "{req}"' + "&sortby=rlv"
+    res = yandex.search(q_text).items
+    for item in res:
+        print(i)
+        file_ans.write(f'<p>{i}. <a href=' + item['url'] + ">" + item['url'][:40] + "</a></p>" + "\n")
+        i += 1
