5c84ae62d815c2c434fbdb52f9d6e32a4ae1d96c
yellowpearl <yellowpearl@yandex.ru>
2020-10-21 14:47:10 +0300

    Done

--- yndx_parser.py
+++ yndx_parser.py
@@ -3,6 +3,10 @@ from conf import cnf
 
 user_id = cnf['user_id']
 key_id = cnf['key_id']
+if cnf['strict'] == True:
+    strict = '"'
+else:
+    strict = ''
 
 file_src = open('sources.txt', 'r')
 file_req = open('request.txt', 'r')
@@ -14,9 +18,9 @@ yandex = yandex_search.Yandex(api_user=user_id, api_key=key_id)
 i = 0
 file_ans = open('ans.html', 'w')
 for source in sources:
-    q_text = f'site:{source} "{req}"' + "&sortby=rlv"
+    q_text = f'site:{source} {strict}{req}{strict}' + "&sortby=rlv"
     res = yandex.search(q_text).items
     for item in res:
         print(i)
-        file_ans.write(f'<p>{i}. <a href=' + item['url'] + ">" + item['url'][:40] + "</a></p>" + "\n")
+        file_ans.write(f'<p>{i}. <a href=' + item['url'] + ">" + item['url'][:60] + "</a></p>" + "\n")
         i += 1
